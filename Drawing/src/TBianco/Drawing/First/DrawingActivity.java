d1faace4e51b18d9e17cf4442d29c52ba940e17d
Tony Bianco <fattony4291@gmail.com>
2013-02-22 14:53:31 -0500

    first upload
    initial files

---
+++ Drawing/src/TBianco/Drawing/First/DrawingActivity.java
@@ -0,0 +1,13 @@
+package TBianco.Drawing.First;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DrawingActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
